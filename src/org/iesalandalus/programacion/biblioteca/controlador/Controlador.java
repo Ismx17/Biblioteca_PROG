b8516507eecfce9168f77c630f318653ab110721
Ismx17 <irosros933@gmail.com>
2026-01-16 19:03:10 +0100

    Creo la clase Controlador

---
+++ src/org/iesalandalus/programacion/biblioteca/controlador/Controlador.java
@@ -0,0 +1,80 @@
+package org.iesalandalus.programacion.biblioteca.controlador;
+
+import java.time.LocalDate;
+
+import org.iesalandalus.programacion.biblioteca.modelo.Modelo;
+import org.iesalandalus.programacion.biblioteca.modelo.dominio.Libro;
+import org.iesalandalus.programacion.biblioteca.modelo.dominio.Prestamo;
+import org.iesalandalus.programacion.biblioteca.modelo.dominio.Usuario;
+import org.iesalandalus.programacion.biblioteca.vista.Vista;
+
+public class Controlador {
+    private Modelo modelo;
+    private Vista vista;
+
+public Controlador(Modelo modelo, Vista vista) {
+        if(modelo == null || vista == null) {
+            throw new IllegalArgumentException("ERROR: Los parametros no pueden ser nulos.");
+        }
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+
+    public void comenzar() {
+        modelo.comenzar();
+        vista.comenzar();
+    }
+
+    public void terminar() {
+        modelo.terminar();
+        vista.terminar();
+    }
+
+    public void alta(Libro libro) {
+        modelo.alta(libro);
+    } 
+
+    public boolean baja(Libro libro) {
+        return modelo.baja(libro);
+    }
+
+    public Libro buscar(Libro libro) {
+        return modelo.buscar(libro);
+    }
+
+    public Libro[] listadoLibros() {
+        return modelo.listadoLibros();
+    }
+
+    public void alta(Usuario usuario) {
+        modelo.alta(usuario);
+    }
+
+    public boolean baja(Usuario usuario) {
+        return modelo.baja(usuario);
+    }
+
+    public Usuario buscar(Usuario usuario) {
+        return modelo.buscar(usuario);
+    }
+
+    public Usuario[] listadoUsuarios() {
+        return modelo.listadoUsuarios();
+    }
+
+    public void prestar(Libro libro, Usuario usuario, LocalDate fecha) {
+        modelo.prestar(libro, usuario, fecha);
+    }
+
+    public boolean devolver(Libro libro, Usuario usuario, LocalDate fecha) {
+        return modelo.devolver(libro, usuario, fecha);
+    }
+
+    public Prestamo[] listadoPrestamos(Usuario usuario) {
+        return modelo.listadoPrestamos(usuario);
+    }
+
+    public Prestamo[] listadoPrestamos() {
+        return modelo.listadoPrestamos();
+    }
+}
