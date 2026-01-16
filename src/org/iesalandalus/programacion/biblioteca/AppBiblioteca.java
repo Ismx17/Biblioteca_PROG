acd303225db701e01ac48e95032194170fefbc46
Ismx17 <irosros933@gmail.com>
2026-01-16 19:06:42 +0100

    Creo la clase AppBiblioteca

--- src/org/iesalandalus/programacion/biblioteca/AppBiblioteca.java
+++ src/org/iesalandalus/programacion/biblioteca/AppBiblioteca.java
@@ -1,7 +1,16 @@
 package org.iesalandalus.programacion.biblioteca;
 
+import org.iesalandalus.programacion.biblioteca.controlador.Controlador;
+import org.iesalandalus.programacion.biblioteca.modelo.Modelo;
+import org.iesalandalus.programacion.biblioteca.vista.Vista;
+
 public class AppBiblioteca {
 
     public static void main(String[] args) {
+        Modelo modelo = new Modelo();
+        Vista vista = new Vista();
+        Controlador controlador = new Controlador(modelo, vista);
+        vista.setControlador(controlador);
+        controlador.comenzar();
     }
 }
