d6501ed32cb88780e55c2c9b98c33ff7b00f5ff3
Ismx17 <irosros933@gmail.com>
2026-04-22 20:31:16 +0200

    Agrego archivos no versionados

---
+++ src/main/java/biblioteca/Main.java
@@ -0,0 +1,8 @@
+package biblioteca;
+
+public class Main {
+    public static void main(String[] args) {
+        // Esto "engaña" al entorno de ejecución y evita la comprobación de módulos
+        AppBiblioteca.main(args);
+    }
+}
