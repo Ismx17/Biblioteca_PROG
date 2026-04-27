62acc2424a7fe2b09b0b7e71886b9c3d53a6e8f2
Ismx17 <irosros933@gmail.com>
2026-04-27 14:11:15 +0200

    Corrijo comentario

--- src/main/java/biblioteca/Main.java
+++ src/main/java/biblioteca/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         /**
          * Este Main engaña al entorno de ejecución y evita la comprobación de módulos
-         * ya que no me ha sido posible lanzarlo directamente usando la clase AppBiblioteca desde el IDE.
+         * porque no me ha sido posible lanzarlo directamente usando la clase AppBiblioteca desde el IDE.
          */
         AppBiblioteca.main(args);
     }
