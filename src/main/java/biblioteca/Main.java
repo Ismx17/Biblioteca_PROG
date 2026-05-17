39df450bc01d09720beb3cf03a8fa2e7d65c6985
irosros <irosros933@gmail.com>
2026-05-17 19:56:59 +0200

    Refactorizo gestor de dependencias gradle

--- src/main/java/biblioteca/Main.java
+++ src/main/java/biblioteca/Main.java
@@ -2,10 +2,6 @@ package biblioteca;
 
 public class Main {
     public static void main(String[] args) {
-        /**
-         * Este Main engaña al entorno de ejecución y evita la comprobación de módulos
-         * porque no me ha sido posible lanzarlo directamente usando la clase AppBiblioteca desde el IDE.
-         */
         AppBiblioteca.main(args);
     }
 }
